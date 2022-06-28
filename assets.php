d986ad244379fc5f471c85949d61dac616c468ed
Mekhi Marolyne <mekiceng99@gmail.com>
2022-06-28 16:53:18 +0800

    done  pdf

--- assets.php
+++ assets.php
@@ -1,76 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>My Assets Report</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-    <style>
-    	body {
-		font-size: 14px;
-		padding: 10px;
-	  }
-      table, th, td {
-    border:1px solid black;
-    }
-    </style>
-</head>
-<body>
-<header>
-<div class="p-3 text-right" >
-<button onclick="window.print()">Print</button>
-</div>
-  <div class="p-5 text-center" >
-    <h1 class="mb-1" style="font-size:22px">
-      <b>Laporan Senarai Inventori</b>
-    </h1>
-  </div>
-</header>
-</body>
-<table>
 <?php
-  $db_host = '127.0.0.1';
-  $db_user = 'root';
-  $db_password = '';
-  $db_name = 'myasset_malaysia'; 
 
-  // Establish DB connections
-  $connection = mysqli_connect($db_host, $db_user, $db_password, $db_name) or die(mysqli_connect_error());
+$oExcel = new PHPExcel();
 
-  /** 
-  * SQL
-  */
-  //Retrieve code and stock description for class 1 and 2
-  $result = mysqli_query($connection, "SELECT DISTINCT class1, stok_desc1 FROM inventori ORDER BY class1");
-  while($row = mysqli_fetch_array($result)) 
-  {
-    echo '<b>'.$row[ "class1"].' '.$row["stok_desc1"].'</b><br>';
+// first row
+$oExcel->getActiveSheet()->setCellValueByColumnAndRow(0, 1, 'id');
+$oExcel->getActiveSheet()->setCellValueByColumnAndRow(1, 1, 'name');
+$oExcel->getActiveSheet()->setCellValueByColumnAndRow(2, 1, 'email');
 
-    $class1_code = $row["class1"];
-    $result1 = mysqli_query($connection, "SELECT DISTINCT class1, class2, stok_desc2 FROM inventori WHERE class1 = '$class1_code'");
+// second row 
+$oExcel->getActiveSheet()->setCellValueByColumnAndRow(0, 2, 230);
+$oExcel->getActiveSheet()->setCellValueByColumnAndRow(1, 2, 'John');
+$oExcel->getActiveSheet()->setCellValueByColumnAndRow(2, 2, 'john@yahoo.com');
 
-    while($row = mysqli_fetch_array($result1)) 
-    {
-      echo '&nbsp&nbsp<b>'.$row[ "class2"].' '.$row["stok_desc2"].'</b><br>';
+// third row 
+$oExcel->getActiveSheet()->setCellValueByColumnAndRow(0, 3, 350);
+$oExcel->getActiveSheet()->setCellValueByColumnAndRow(1, 3, 'Mark');
+$oExcel->getActiveSheet()->setCellValueByColumnAndRow(2, 3, 'mark@yahoo.com');
 
-      $class2_code = $row["class2"];
-      $result2 = mysqli_query($connection, "SELECT DISTINCT class1, class2, class3, stok_desc3 FROM inventori WHERE class1 = '$class1_code' AND class2 = '$class2_code'");
-      while($row = mysqli_fetch_array($result2)) 
-      {
-        echo '&nbsp&nbsp&nbsp&nbsp<b>'.$row[ "class3"].' '.$row["stok_desc3"].'</b><br>';
+$objWriter = PHPExcel_IOFactory::createWriter($oExcel, 'Excel5');
 
-        $class3_code = $row["class3"];
-        $result3 = mysqli_query($connection, "SELECT DISTINCT class1, class2, class3, code, stok_desc4 FROM inventori WHERE class1 = '$class1_code' AND class2 = '$class2_code' AND class3 =  $class3_code");
-        while($row = mysqli_fetch_array($result3)) 
-        {
-        echo '&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp'.$row[ "code"].' '.$row["stok_desc4"].'<br>';
-        }
-      }
-     }
-  }
+header('Content-type: application/vnd.ms-excel');
+header('Content-Disposition: attachment; filename="export_'. date('Y-m-d').'.xls"');
+header("Pragma: no-cache");
+header("Expires: 0");
 
-?>
-</table>
-</html>
+$objWriter->save('php://output');
--- myassets.php
+++ myassets.php
@@ -15,12 +15,18 @@
       table, th, td {
     border:1px solid black;
     }
+
+    @media print{
+      .dontdisplay{
+        display:none;
+      }
+    }
     </style>
 </head>
 <body>
 <header>
 <div class="p-3 text-right" >
-<button onclick="window.print()">Print</button>
+<button onclick="window.print()" class="dontdisplay">Print</button>
 </div>
   <div class="p-5 text-center" >
     <h1 class="mb-1" style="font-size:22px">
@@ -29,12 +35,7 @@
   </div>
 </header>
 </body>
-<<<<<<< HEAD
 
-=======
-<table>
-  <td>
->>>>>>> master
 <?php
   $db_host = '127.0.0.1';
   $db_user = 'root';
@@ -43,43 +44,10 @@
 
   // Establish DB connections
   $connection = mysqli_connect($db_host, $db_user, $db_password, $db_name) or die(mysqli_connect_error());
-<<<<<<< HEAD
-?>
-    <?php
-=======
-
->>>>>>> master
   /** 
   * SQL
   */
   //Retrieve code and stock description for class 1 and 2
-<<<<<<< HEAD
-  $result = mysqli_query($connection, "SELECT class1, stok_desc1 FROM inventori");
-
-  while($row = mysqli_fetch_array($result)) 
-  {
-      echo '<b>'.$row[ "class1"].' '.$row["stok_desc1"].'</b><br>';
-
-      $result = mysqli_query($connection, "SELECT class1, class2, stok_desc2 FROM inventori");
-      while($row = mysqli_fetch_array($result)) 
-      {
-      echo '&nbsp&nbsp'.$row[ "class2"].' '.$row["stok_desc2"].'<br>';
-
-      $result = mysqli_query($connection, "SELECT class1, class2, class3, stok_desc3 FROM inventori");
-      while($row = mysqli_fetch_array($result)) 
-      {
-      echo '&nbsp&nbsp&nbsp&nbsp'.$row[ "class3"].' '.$row["stok_desc3"].'<br>';
-
-      $result = mysqli_query($connection, "SELECT class1, class2, class3, code, stok_desc4 FROM inventori WHERE class1 = '101' AND class2='001' AND class3 = '001'  ");
-      while($row = mysqli_fetch_array($result)) 
-      {
-      echo '&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp'.$row[ "code"].' '.$row["stok_desc4"].'<br>';
-    }
-  }
-  }
-  }
-    ?>
-=======
   $result = mysqli_query($connection, "SELECT DISTINCT class1, stok_desc1 FROM inventori ORDER BY class1");
   while($row = mysqli_fetch_array($result)) 
   {
@@ -90,26 +58,23 @@
 
     while($row = mysqli_fetch_array($result1)) 
     {
-      echo '&nbsp&nbsp<b>'.$row[ "class2"].' '.$row["stok_desc2"].'</b><br>';
+      echo '&emsp;&emsp;<b>'.$row[ "class2"].' '.$row["stok_desc2"].'</b><br>';
 
       $class2_code = $row["class2"];
       $result2 = mysqli_query($connection, "SELECT DISTINCT class1, class2, class3, stok_desc3 FROM inventori WHERE class1 = '$class1_code' AND class2 = '$class2_code'");
       while($row = mysqli_fetch_array($result2)) 
       {
-        echo '&nbsp&nbsp&nbsp&nbsp<b>'.$row[ "class3"].' '.$row["stok_desc3"].'</b><br>';
+       echo '&emsp;&emsp;&emsp;&emsp;<b>'.$row[ "class3"].' '.$row["stok_desc3"].'</b><br>';
 
         $class3_code = $row["class3"];
         $result3 = mysqli_query($connection, "SELECT DISTINCT class1, class2, class3, code, stok_desc4 FROM inventori WHERE class1 = '$class1_code' AND class2 = '$class2_code' AND class3 =  $class3_code");
         while($row = mysqli_fetch_array($result3)) 
         {
-        echo '&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp'.$row[ "code"].' '.$row["stok_desc4"].'<br>';
+         echo '&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;'.$row[ "code"].' '.$row["stok_desc4"].'<br>';
         }
       }
      }
   }
 
 ?>
-</td>
-</table>
->>>>>>> master
 </html>
